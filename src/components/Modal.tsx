a69d61ae9a2528a2200cd73fe3bff2930396f313
Camomilte <camomilte@gmail.com>
2025-11-12 15:30:42 +0100

    counters done, booking modal functioning

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,11 +11,13 @@ export const Modal = ({ open, onClose, children }: ModalProps) => {
   if(!open) return null;
 
   return (
-    <div className="fixed inset-0 bg-black/50 flex items-end justify-center z-50">
-      <div className="bg-br-secondary w-full min-h-9/10 rounded-t-3xl p-4 relative">
-        <button onClick={onClose} className="btn-secondary icon-sm absolute right-4">
-          <IoClose className="size-full"/>
-        </button>
+    <div className="fixed inset-0 bg-black/50 flex items-end justify-center z-50 lg:items-center">
+      <div className="bg-br-secondary w-full min-h-9/10 rounded-t-3xl p-4 relative lg:max-w-lg lg:min-h-auto lg:rounded-3xl">
+        <div className="w-full flex justify-end">
+          <button onClick={onClose} className="btn-secondary icon-sm">
+            <IoClose className="size-full"/>
+          </button>
+        </div>
         {children}
       </div>
 
--- src/features/booking/components/BookingCard.tsx
+++ src/features/booking/components/BookingCard.tsx
@@ -3,6 +3,9 @@ import { Modal } from "../../../components/Modal";
 import { DayPicker } from "react-day-picker";
 import { FaMinus, FaPlus } from "react-icons/fa6";
 
+//TODO: Check so start day cannot be before today and end day cannot be before start day
+
+//TODO: Change button to set guests
 
 
 export const BookingCard = () => {
@@ -13,7 +16,35 @@ export const BookingCard = () => {
   const [checkOut, setCheckOut] = useState<Date | undefined>(undefined);
 
   const [guestsModalOpen, setGuestsModalOpen] = useState(false);
-  const [guests, setGuests] = useState<[] | undefined>(undefined);
+
+  const [guestsCounters, setGuestsCounters] = useState({
+    adults: { value: 1, description: "18 and older" },
+    children: { value: 0, description: "2-17 years old" },
+    infants: { value: 0, description: "Younger than 2 years old" },
+    pets: { value: 0, description: "Excluding service animals" }
+  });
+
+  const handleIncrement = (key: keyof typeof guestsCounters) => {
+    setGuestsCounters(prev => ({ 
+      ...prev, 
+      [key]: {
+        ...prev[key],
+        value: prev[key].value + 1,
+      }
+    }));
+  }
+
+  const handleDecrement = (key: keyof typeof guestsCounters) => {
+    setGuestsCounters(prev => ({ 
+      ...prev, 
+      [key]: {
+        ...prev[key],
+        value: Math.max(prev[key].value - 1,0),
+      } 
+    }));
+  }
+
+
 
   return (
     <div>
@@ -84,20 +115,46 @@ export const BookingCard = () => {
         <Modal open={guestsModalOpen} onClose={() => setGuestsModalOpen(false)}>
           <h3 className="text-3xl font-title font-semibold text-brand">Select Guests</h3>
 
-          <form>
-            <label htmlFor="adults">Adults</label>
-            <div>
-              <button>
-                <FaMinus />
-              </button>
-              <input type="number" value={1}/>
-              <button>
-                <FaPlus />
-              </button>
-            </div>
+          <form className="py-4">
+            {Object.keys(guestsCounters).map(key => {
+              const counter = guestsCounters[key as keyof typeof guestsCounters]
+              return (
+                <div key={key} className="flex justify-between items-center py-4">
+                  <div>
+                    <label htmlFor={key} className="font-bold capitalize">{key}</label>
+                    <p className="text-neutral-500">{counter.description}</p>
+
+                  </div>
+                  <div className="flex items-center py-2">
+                    <button
+                      type="button"
+                      onClick={() => handleDecrement(key as keyof typeof guestsCounters)}
+                      className="icon-sm size-8! btn-secondary"
+                    >
+                      <FaMinus />
+                    </button>
+
+                    <input 
+                      id={key}
+                      type="number"
+                      readOnly
+                      value={counter.value}
+                      className="text-center max-w-14" 
+                    />
+
+                    <button
+                      type="button"
+                      onClick={() => handleIncrement(key as keyof typeof guestsCounters)}
+                      className="icon-sm size-8! btn-secondary"
+                    >
+                      <FaPlus />
+                    </button>
+                  </div>
+                </div>
+              )
+                
+            })}
           </form>
-
-
         </Modal>
         
         <a href="#">
