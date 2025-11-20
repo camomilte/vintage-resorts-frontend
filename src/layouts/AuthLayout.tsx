e7cf748e2bf7297bae578f1a78abc3a0a6221571
Camomilte <camomilte@gmail.com>
2025-11-20 12:27:29 +0100

    minor styling

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router"
 import { CompactNav } from "../components/CompactNav"
-import Footer from "../components/Footer"
 import { StickyNav } from "../components/StickyNav"
 
 export const AuthLayout = () => {
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -3,8 +3,16 @@ import { RegisterForm } from '../features/authentication/components/RegisterForm
 function Register() {
   return (
     <div className='px-4 mt-10 lg:px-24'>
-      <div>
-        <h1>Create an account!</h1>
+      <div className='md:max-w-lg md:mx-auto'>
+        <h1 className="font-title font-semibold text-brand text-4xl">Create an account!</h1>
+        <div className='my-2'>
+          <p>
+            Already have an account? Login&nbsp;
+            <a href="/auth/login" className="underline underline-offset-3 text-brand cursor-pointer">
+              here!
+            </a>
+          </p>
+        </div>
         <RegisterForm />
       </div>
     </div>
